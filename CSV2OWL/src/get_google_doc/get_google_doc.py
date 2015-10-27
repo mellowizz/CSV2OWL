d4d19bdc450c43d91ce7e2d751d2a836eb015e6d
T. Niklas Moran <moran@campus.tu-berlin.de>
2015-10-28 00:12:30 +0100

    some changes to get-google-doc

--- CSV2OWL/src/get_google_doc/get_google_doc.py
+++ CSV2OWL/src/get_google_doc/get_google_doc.py
@@ -9,25 +9,34 @@ import requests
 import shutil
 import sys
 import io
-import os
-#from os.path import expanduser
+# from os.path import expanduser
 '''master version '''
-response = requests.get('https://docs.google.com/feeds/download/spreadsheets/Export?key=1DetLdQWehIFy31pFduqU20o_EO8KziKxWiORkhXzzQc&exportFormat=csv&gid=403134972', stream=True)
+
+google_url = 'https://docs.google.com/feeds/download/spreadsheets/Export?'
+# response = requests.get('https://docs.google.com/feeds/download/
+# spreadsheets/'+
+# 'Export?key=1DetLdQWehIFy31pFduqU20o_EO8KziKxWiORkhXzzQc&exportFormat
+# =csv&gid=403134972', stream=True)
+file_key = '1DetLdQWehIFy31pFduqU20o_EO8KziKxWiORkhXzzQc'
+file_format = 'csv'
+file_gid = '403134972'
+options = 'key={}&exportFormat={}&gid={}'.format(file_key, file_format,
+                                                 file_gid)
+full_url = ''.join([google_url, options])
+response = requests.get(full_url, stream=True)
 ''' simons version'''
-#reponse = requests.get('https://docs.google.com/feeds/download/spreadsheets/Export?key=1oa4IThXlJa-1VXXtQs5AApHl0qEyPZd-p5RPiy6P9vk&exportFormat=csv&gid=1755891071', stream=True)
+# reponse = key=1oa4IThXlJa-1VXXtQs5AApHl0qEyPZd-p5RPiy6P9vk
+# &exportFormat=csv&
+# gid=1755891071', stream=True)
 response.raise_for_status()
 
-#home = os.path.expanduser("~")
-
-#output_file = os.path.join(home, "test-rlp", "rlp_key_latest.csv")
-#output_file = output_dir + rlp_key_latest.csv'
 output_file = sys.argv[1]
 print(output_file)
 if sys.version < '3':
     infile = io.open(output_file, 'wb')
 else:
     infile = io.open(output_file, 'wb')
-#print(response.content)
+# print(response.content)
 with infile as csv:
     response.raw.decode_content = True
     shutil.copyfileobj(response.raw, csv)
